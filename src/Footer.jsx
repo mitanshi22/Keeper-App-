f75325195e9cd2febb59fdf57a84c4f9869b2366
NamanYadav11 <naman198323@gmail.com>
2024-10-22 23:26:44 +0530

    bug fixed

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 function Footer() {
     const currentYear = new Date().getFullYear();
-    return <Footer>
-        Copyright © {currentYear}
-    </Footer>
+    return (
+        <footer>
+            Copyright © {currentYear}
+        </footer>
+    )
 }
 
 export default Footer;
